58e36c781512cda27b94c87a3d335892842de32d
steven <msycomtw@gmail.com>
2023-09-20 16:08:13 +0800

    feat: add develop assignment

--- project.py
+++ project.py
@@ -1,7 +1,15 @@
 """
-version 1.0
+Bracnn from version 1.0
 """
 
 # 穩定版本功能 - 印出1~10
 for i in range(10):
     print(i)
+    
+# 欲開發的功能 Feture 1 - 印出1~10的平方
+
+
+# 欲開發的功能 Feture 2 - 印出1~10的立方
+
+
+# 欲開發的功能 Feture 3 - 印出1~10的兩倍
